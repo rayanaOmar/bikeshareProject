1ac95dc56710ec9e14d1ab2ce98e712bb01ca541
rayanaOmar <61861384+rayanaOmar@users.noreply.github.com>
2022-12-09 21:57:04 +0300

    refactor: Added a print statements in the end of the code

--- bikeshare.py
+++ bikeshare.py
@@ -221,7 +221,7 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+	
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
@@ -231,6 +231,8 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n').lower()
         if restart.lower() != 'yes':
             break
+	
+	print("The End")
 
 if __name__ == "__main__":
 	main()
